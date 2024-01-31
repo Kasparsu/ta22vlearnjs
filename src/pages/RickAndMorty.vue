4752ff1b1f59dac57754b9d986ffcba63f1f0562
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-01-31 09:56:06 +0200

    Add infinite scroll

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -1,24 +1,4 @@
 <template>
-    <div class="field has-addons">
-        <p class="control">
-            <button :disabled="!info.prev" class="button is-primary" @click="prev">
-                Prev
-            </button>
-        </p>
-        <p v-for="label in generatePagination(page, info.pages)" class="control">
-            <button 
-                class="button" 
-                :class="{'is-static': label==='...', 'is-primary': label===page }"
-                @click="getPage(label)">
-                {{ label }}
-            </button>
-        </p>
-        <p class="control">
-            <button :disabled="!info.next" class="button is-primary"  @click="next">
-                Next
-            </button>
-        </p>
-    </div>
     <div class="columns is-multiline">
         <div v-for="char in chars" class="column is-one-quarter">
             <CharacterCard :character="char"></CharacterCard>
@@ -28,7 +8,7 @@
 <script setup>
 import CharacterCard from '../components/CharacterCard.vue';
 import axios from 'axios';
-import { ref, reactive } from 'vue';
+import { ref, reactive, onMounted } from 'vue';
 let info = ref({});
 let chars = ref([]);
 let page = ref(1);
@@ -38,42 +18,23 @@ function getCharacters(url){
     axios.get(url).then(response => {
         console.log(response.data);
         info.value = response.data.info;
-        chars.value = response.data.results
+        chars.value.push(...response.data.results)
         
     });
-}
-
-function getPage(value){
-    getCharacters('https://rickandmortyapi.com/api/character?page=' + value);
-    page.value = value;
 }
 
 function next(){
     getCharacters(info.value.next);
     page.value++;
 }
-function prev(){
-    getCharacters(info.value.prev);
-    page.value--;
-}
+onMounted(()=>{
+    console.log(window);
 
-function generatePagination(current,total){
-    let pages = [];
-    for(let i=1;i<=3;i++){
-        pages[i] = i;
-    }
-    if(current>2 && current<total-1){
-        pages.push('...');
-        for(let i=current-1;i<=current+1;i++){
-            pages[i] = i;
+    document.addEventListener('scroll', () => {
+        if(window.scrollY+window.innerHeight > document.body.clientHeight - 200){
+            next();
         }
-    }
-    pages.push('...');
-    for(let i=total-2;i<=total;i++){
-        pages[i] = i;
-    }
-    return pages.filter(val => val);
-}
+    })
+});
 
-console.log(generatePagination(41, 42));
 </script>
