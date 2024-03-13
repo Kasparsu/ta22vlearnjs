6dcb762ff6ef2b9c0497073f0414c6e99fb66f61
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-03-13 09:56:05 +0200

    scrape json

--- bin/terminal.mjs
+++ bin/terminal.mjs
@@ -5,28 +5,38 @@ const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 if(!fs.existsSync('cache')){
     fs.mkdirSync('cache');
 }
-const baseUrl = 'https://wumo.com';
-let link = '/wumo/2024/03/13';
-for(let i = 0; i<10; i++){
-    let cacheName = link.replaceAll('/', '')
-    let cachePath = `cache/${cacheName}.html`;
+
+for(let i = 1; i<10; i++){
+    let cachePath = `cache/${i}.json`;
     let data;
     if(!fs.existsSync(cachePath)){
         await sleep(1000);
-        let res = await axios.get(baseUrl + link);
+        let res = await axios.post('https://kinnisvara24.ee/search', {
+            page: i
+        });
         //CACHE
-        fs.writeFileSync(cachePath, res.data);
+        fs.writeFileSync(cachePath, JSON.stringify(res.data));
         data = res.data;
         console.log("LIVE REQUEST!");
     } else {
-        data = fs.readFileSync(cachePath);
+        data = JSON.parse(fs.readFileSync(cachePath));
     }
-    const $ = cheerio.load(data);
-    let img = $('article.wumo img');
-    console.log(img.attr('src'));
-    console.log(img.attr('alt'));
-    let prev = $('a.prev');
-    link = prev.attr('href');
+    //console.log(data);
+    data.data.forEach(ad => {
+        if(ad.address.address){
+            console.log(ad.address.address, ad.hind);
+        } else {
+            let address = ad.address.A1 + ',' + 
+                            ad.address.A2 + ',' +
+                            ad.address.A3 + ',' +
+                            ad.address.A4 + ',' +
+                            ad.address.A5 + ',' +
+                            ad.address.A6 + ',' +
+                            ad.address.A7 + ',' +
+                            ad.address.A8 + ','
+            console.log(address, ad.hind);            
+        }
+    });
     
 }
 
